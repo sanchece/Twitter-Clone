5eb23744bb2825f186765583b5edd6c00685f2f9
Your Name <csanchez112598@gmail.com>
2021-04-30 20:00:22 -0400

    completed part 2

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 toolbar = DebugToolbarExtension(app)
 
@@ -179,6 +179,13 @@ def users_followers(user_id):
     user = User.query.get_or_404(user_id)
     return render_template('users/followers.html', user=user)
 
+@app.route('/users/<int:user_id>/likes')
+def users_likes(user_id):
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
+    return render_template('users/likes.html',user=g.user)
+
 
 @app.route('/users/follow/<int:follow_id>', methods=['POST'])
 def add_follow(follow_id):
@@ -229,6 +236,17 @@ def profile():
 
     return render_template('users/edit.html',form=form,user_id=g.user.id)
 
+@app.route('/users/add_like/<int:message_id>', methods=["POST"]) 
+def like_message(message_id):
+    liked_message=Message.query.get_or_404(message_id)
+    if liked_message in g.user.likes:
+        g.user.likes.remove(liked_message)
+        db.session.commit()
+        return redirect('/')
+    
+    g.user.likes.append(liked_message)
+    db.session.commit()
+    return redirect('/')
 
 
 
